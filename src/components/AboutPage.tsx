7beb0aa54e78b355e2700675df561df5054fd1f1
pooria <abagheri1994@gmail.com>
2024-04-22 10:52:27 +0330

    feat: Add education list

--- src/components/AboutPage.tsx
+++ src/components/AboutPage.tsx
@@ -1,6 +1,8 @@
 import PageContainer from './common/PageContainer';
 import { experiences } from '@/data/experiences';
 import ExperienceList from './common/ExperienceList';
+import EducationList from './common/EducationList';
+import { educations } from '@/data/educations';
 
 export default function AboutPage() {
   return (
@@ -8,6 +10,7 @@ export default function AboutPage() {
       <h1 className="text-4xl font-bold">About Me</h1>
       <p className="mt-4">Hello, I am Alireza Bagheri.</p>
       <ExperienceList data={experiences} />
+      <EducationList data={educations} />
     </PageContainer>
   );
 }
---
+++ src/components/common/EducationList.tsx
@@ -0,0 +1,50 @@
+import ImageWithFallback from '@/kit/ImageWithFallback';
+import LinkOrBox from '@/kit/LinkOrBox';
+import TimelineSection, { TimelineBox } from './TimelineSection';
+import { faGraduationCap } from '@fortawesome/free-solid-svg-icons';
+import educationPlaceholder from '@images/company/education-placeholder.png';
+import TimelineDateRange from './TimelineDateRange';
+import { Education } from '@/data/educations';
+
+export default function EducationList({ data }: { data: Education[]; }) {
+  return (
+    <TimelineSection title="Education" icon={faGraduationCap}>
+      {data.map((item) => <EducationItem key={item.slug} data={item} />)}
+    </TimelineSection>
+  );
+}
+
+export function EducationItem({ data }: { data: Education; }) {
+  const {
+    startDate, endDate, school, description, field, degree,
+  } = data;
+  return (
+    <TimelineBox>
+      <TimelineDateRange startDate={startDate} endDate={endDate} />
+      <div className="flex items-start gap-4">
+        <LinkOrBox href={school?.link} target="_blank" className="shrink-0">
+          <ImageWithFallback
+            src={school?.logo}
+            fallback={educationPlaceholder}
+            alt={school?.title || ''}
+            sizes="40px"
+            width={40}
+            height={40}
+            className="object-contain w-10 h-10 rounded-lg"
+          />
+        </LinkOrBox>
+        <div className="flex flex-col">
+          <h3 className="text-lg font-bold">
+            <LinkOrBox href={school?.link} target="_blank" className="inline">
+              {school?.title}
+            </LinkOrBox>
+          </h3>
+          <h4 className="text-sm text-bodySecondary">
+            {degree}, {field}
+          </h4>
+          <p className="text-sm text-bodySecondary">{description}</p>
+        </div>
+      </div>
+    </TimelineBox>
+  );
+}
