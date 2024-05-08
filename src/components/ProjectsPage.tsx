142057bba8628b721dec0dcfc0b1f3cfc1971420
pooria <abagheri1994@gmail.com>
2024-05-08 16:29:57 +0200

    feat: Add Initial projects page

--- src/components/ProjectsPage.tsx
+++ src/components/ProjectsPage.tsx
@@ -1,10 +1,16 @@
+import { projects } from '@/data/projects';
 import PageContainer from './common/PageContainer';
+import ProjectItem from './common/ProjectItem';
 
 export default function ProjectsPage() {
   return (
     <PageContainer>
       <h1 className="text-4xl font-bold">Projects</h1>
-      <p className="mt-4">My projects.</p>
+      <div className="flex flex-col gap-4 my-8">
+        {projects.map((project) => (
+          <ProjectItem key={project.slug} data={project} />
+        ))}
+      </div>
     </PageContainer>
   );
 }
---
+++ src/components/common/ProjectItem.tsx
@@ -0,0 +1,39 @@
+import { Project } from '@/data/projects';
+
+export default function ProjectItem({ data }: { data: Project; }) {
+  const { slug, title, date, description, links, points, tools, gallery } = data;
+  return (
+    <div className="py-4 border-t">
+      <h2 className="text-2xl font-bold">
+        {title}
+        {date && <span className="ms-2 font-normal text-xs text-bodySecondary">{date}</span>}
+      </h2>
+      {links && (
+        <ul className="flex gap-2">
+          {links.map((link, i) => (
+            <li key={i}>
+              <a href={link} target="_blank" rel="noreferrer" className="text-xs text-primary hover:underline">{link}</a>
+            </li>
+          ))}
+        </ul>
+      )}
+      {description && (
+        <p className="text-sm my-2 text-bodySecondary">{description}</p>
+      )}
+      {tools && (
+        <ul className="flex flex-wrap gap-2 items-center my-2">
+          {tools.map((tool, i) => (
+            <li className="text-xs font-semibold p-1 px-2 bg-primary/25 rounded-full" key={i}>{tool}</li>
+          ))}
+        </ul>
+      )}
+      {points && (
+        <ul className="list-disc list-inside mt-2">
+          {points.map((point, i) => (
+            <li className="text-sm text-bodySecondary" key={i}>{point}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
