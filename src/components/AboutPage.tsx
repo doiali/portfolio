1e980e28568320b21981d347e7ea0a6ddb43199c
pooria <abagheri1994@gmail.com>
2024-05-13 16:48:17 +0200

    feat: Add introduction video to about page

--- src/components/AboutPage.tsx
+++ src/components/AboutPage.tsx
@@ -5,16 +5,31 @@ import EducationList from './common/EducationList';
 import { educations } from '@/data/educations';
 import HonorsList from './common/HonorsList';
 import { honors } from '@/data/honors';
+import Image from 'next/image';
 
 export default function AboutPage() {
   return (
     <PageContainer>
       <h1 className="text-4xl font-bold">About Me</h1>
-      <p className="mt-4 max-w-2xl">
-        Hi, I&apos;m Alireza Bagheri, 29 years old with over 4 years of experience as a Senior Frontend Developer. I specialize in ReactJS and Next.js.<br />
-        I started programming during my mechanical engineering studies because I really liked it. For my master’s degree, I studied Mechatronics Engineering and worked on flying robots. After finishing school, I decided to become a web developer because I found it very interesting. <br />
-        Today, I am proud of the great projects I have worked on. I am excited to keep working as a developer, make fast and beautiful applications, and learn more new things along the way.
-      </p>
+      <div className="mt-4">
+        <video
+          className="w-[min(100%,500px)] mx-auto mb-2 lg:float-end lg:ms-4"
+          poster="/videos/introduction.jpg"
+          width="1920"
+          height="1080"
+          controls
+          preload="none"
+        >
+          <source src="/videos/introduction.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <p className="order-2 text-lg mt-4">
+          Hi, I&apos;m Alireza Bagheri, 29 years old with over 4 years of experience as a Senior Frontend Developer. I specialize in ReactJS and Next.js.<br />
+          I started programming during my mechanical engineering studies because I really liked it. For my master’s degree, I studied Mechatronics Engineering and worked on flying robots. After finishing school, I decided to become a web developer because I found it very interesting. <br />
+          Today, I am proud of the great projects I have worked on. I am excited to keep working as a developer, make fast and beautiful applications, and learn more new things along the way.
+        </p>
+        <br className="clear-both" />
+      </div>
       <ExperienceList data={experiences} />
       <EducationList data={educations} />
       <HonorsList data={honors} />
