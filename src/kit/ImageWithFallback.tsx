1d08ac47214818d3b0c648ecf76ec9981d0b038d
pooria <abagheri1994@gmail.com>
2024-12-13 17:58:51 +0100

    refactor: Migrate to latest versions

--- src/kit/ImageWithFallback.tsx
+++ src/kit/ImageWithFallback.tsx
@@ -8,30 +8,23 @@ export type ImageWithFallbackProps =
     fallback: ImageProps['src'];
   };
 
-type ImageWithFallbackComponent = (
-  props: ImageWithFallbackProps & {
-    ref?: Ref<HTMLImageElement>;
-  }
-) => React.ReactNode;
+function ImageWithFallback(
+  { src, fallback, alt, onError, ...rest }: ImageWithFallbackProps,
+  ref?: Ref<HTMLImageElement>,
+) {
+  const [isError, setIsError] = useState(false);
+  return (
+    <Image
+      src={(isError ? fallback : src) ?? fallback}
+      alt={alt}
+      {...rest} ref={ref}
+      onError={(e) => {
+        setIsError(true);
+        onError?.(e);
+      }}
+    />
+  );
+}
 
-const ImageWithFallback: ImageWithFallbackComponent = React.forwardRef(
-  function ImageWithFallback(
-    { src, fallback, alt, onError, ...rest }: ImageWithFallbackProps,
-    ref?: Ref<HTMLImageElement>,
-  ) {
-    const [isError, setIsError] = useState(false);
-    return (
-      <Image
-        src={(isError ? fallback : src) ?? fallback}
-        alt={alt}
-        {...rest} ref={ref}
-        onError={(e) => {
-          setIsError(true);
-          onError?.(e);
-        }}
-      />
-    );
-  }
-);
 
 export default ImageWithFallback;
